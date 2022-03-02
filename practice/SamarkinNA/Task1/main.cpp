1153b54435c40e88f913f834f03d2e9ecbb76f80
Nikita Sam <work.nicitons@yandex.ru>
2022-03-02 21:04:47 +0300

    Replaced massives with vectors in task1

--- practice/SamarkinNA/Task1/main.cpp
+++ practice/SamarkinNA/Task1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "United.hpp"
 
 template <typename AnyType>
@@ -20,9 +21,9 @@ int main()
 {
 	United First {34212, 'Z', -9.9991};
 	United Second {1, 'd', 23.01};
-	int ints[] = {223, -12};
-	char chars[] = {'z', 'G'};
-	double doubles[] = {2.002, 3.019};
+	std::vector<int> ints = {223, -12};
+	std::vector<char> chars = {'z', 'G'};
+	std::vector<double> doubles = {2.002, 3.019};
 
 	std::cout << "United vars: ";
 	First.printAll();
@@ -30,11 +31,11 @@ int main()
 	Second.printAll(true);
 
 	std::cout << "Min of them is ";
-	( min(First, Second) ).printAll(true);
+	(min(First, Second)).printAll(true);
 
-	printMin(ints[0], ints[1]);
-	printMin(chars[0], chars[1]);
-	printMin(doubles[0], doubles[1]);
+	printMin(ints.at(0), ints.at(1));
+	printMin(chars.at(0), chars.at(1));
+	printMin(doubles.at(0), doubles.at(1));
 
 	return 0;
 }
